9c22c1f60cd41b29bb686c2edad8ec7be3236e55
Rohini Kadu <Rohini Kadu>
2020-04-06 03:33:00 +0530

    Updates

---
+++ precache-manifest.73cef52c56d1e9153eb6eb54ef0b1389.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3608148648e0615fdc34b2e47ef7ea4f",
+    "url": "/gutenberg/index.html"
+  },
+  {
+    "revision": "3ab35d2690cecf53ff1b",
+    "url": "/gutenberg/static/css/main.4db3cdfa.chunk.css"
+  },
+  {
+    "revision": "e7e41dd706dc9be1ebdd",
+    "url": "/gutenberg/static/js/2.88ff02e2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/gutenberg/static/js/2.88ff02e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ab35d2690cecf53ff1b",
+    "url": "/gutenberg/static/js/main.e1963f29.chunk.js"
+  },
+  {
+    "revision": "ea7af7eb1b665ef4f33c",
+    "url": "/gutenberg/static/js/runtime-main.e619ba0d.js"
+  },
+  {
+    "revision": "4a39962b5306eef27e249d24de3c6364",
+    "url": "/gutenberg/static/media/Adventure.4a39962b.svg"
+  },
+  {
+    "revision": "650ced7492e9b4da4e473cdfde0e7aab",
+    "url": "/gutenberg/static/media/Back.650ced74.svg"
+  },
+  {
+    "revision": "ffe5b2615b910b0a79cc8011ee7ef964",
+    "url": "/gutenberg/static/media/Drama.ffe5b261.svg"
+  },
+  {
+    "revision": "fb8af2708ea38a143a34b8f248b57463",
+    "url": "/gutenberg/static/media/Fiction.fb8af270.svg"
+  },
+  {
+    "revision": "0689e2d1c23d48e1c0206075d121bd3f",
+    "url": "/gutenberg/static/media/History.0689e2d1.svg"
+  },
+  {
+    "revision": "5218701c2d69690ae889cfec09c26176",
+    "url": "/gutenberg/static/media/Humour.5218701c.svg"
+  },
+  {
+    "revision": "aa84e2745e1657cb1aed779f4485641a",
+    "url": "/gutenberg/static/media/Next.aa84e274.svg"
+  },
+  {
+    "revision": "10f528b0a49efbd5e38d5f518f667a24",
+    "url": "/gutenberg/static/media/Pattern.10f528b0.svg"
+  },
+  {
+    "revision": "faf4e2b34170f89e2704da510960d46c",
+    "url": "/gutenberg/static/media/Philosophy.faf4e2b3.svg"
+  },
+  {
+    "revision": "bb1d383211ac28a293afbe9619c2dade",
+    "url": "/gutenberg/static/media/Politics.bb1d3832.svg"
+  },
+  {
+    "revision": "8492e7a8993b7335d094584dba64a38b",
+    "url": "/gutenberg/static/media/Search.8492e7a8.svg"
+  }
+]);
